c4e2f6988fcf2c6d57d13e19f8693c8c30f0ac66
re-monu <osndl.monur@aicofindia.com>
2025-05-23 14:37:32 +0530

    change admin component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./component/AdminLogin";
-// import AdminDashboard from "./component/AdminDashboard";
-// import UserForm from "./component/UserForm";
-// import ProductDetail from "./pages/ProductDetail";
-// import CartPage from "./pages/CartPage";
+import AdminDashboard from "./component/AdminDashboard";
+import UserForm from "./component/UserForm";
+import ProductDetail from "./pages/ProductDetail";
+import CartPage from "./pages/CartPage";
 import { CartProvider } from "./context/CartContext";
 import ProductCardPage from "./pages/ProductCardPage";
 import { ToastContainer } from "react-toastify";
-// import UserCreateForm from "./component/UserCreateForm";
+import UserCreateForm from "./component/UserCreateForm";
 import { AuthProvider } from "./context/AuthContext";
 import AuthGuard from "./component/AuthGuard";
 import Homelayout from "./layout/index";
 import "./App.css";
-// import PixiGame from "./PixiFIles/PixiGame";
+import PixiGame from "./PixiFIles/PixiGame";
 function App() {
   return (
     <BrowserRouter
@@ -25,18 +25,8 @@ function App() {
       <AuthProvider>
         <CartProvider>
           <Routes>
-            <Route
-              path="/"
-              element={
-                <AuthGuard>
-                  <Homelayout>
-                    <ProductCardPage />
-                  </Homelayout>
-                </AuthGuard>
-              }
-            />
             <Route path="/login" element={<AdminLogin />} />
-            {/* <Route
+            <Route
               path="/admin/dashboard"
               element={
                 <AuthGuard>
@@ -78,7 +68,7 @@ function App() {
               }
             />
             <Route path="/create-user" element={<UserCreateForm />} />
-            <Route path="/pixi-game" element={<PixiGame />} /> */}
+            <Route path="/pixi-game" element={<PixiGame />} />
           </Routes>
         </CartProvider>
       </AuthProvider>
